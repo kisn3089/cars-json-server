91564b820359f83db4ab5cb68ca12e0bddfcafc4
Stemps <kisn3089@naver.com>
2024-08-30 11:37:35 +0900

    [Feat]server rewriter 변경

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const rewriter = jsonServer.rewriter("routes.json");
+const rewriter = jsonServer.rewriter({
+  "/api/*": "/$1",
+  "/carClasses/:carClassId": "/carClass?carClassId=:carClassId",
+});
 
 server.use(middlewares);
-server.use(
-  rewriter({
-    "/api/*": "/$1",
-  })
-);
+server.use(rewriter);
 server.use(router);
 
 const port = process.env.PORT || 8080;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
+
+// "/carClasses/:carClassId": "/carClass?carClassId=:carClassId",
